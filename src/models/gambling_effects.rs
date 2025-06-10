2122221ca86761e1fa83f3ce8b4c2a431588ad82
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-06-10 21:21:57 +0100

    fix boost stacking

--- src/models/gambling_effects.rs
+++ src/models/gambling_effects.rs
@@ -3,6 +3,7 @@ use chrono::{NaiveDateTime, Utc};
 use serenity::all::UserId;
 use sqlx::{Database, Pool, any::AnyQueryResult};
 
+use crate::ShopPage;
 use crate::shop::{SHOP_ITEMS, ShopItem};
 
 #[async_trait]
@@ -20,10 +21,16 @@ pub trait EffectsManager<Db: Database> {
 
     async fn remove_effect(conn: &mut Db::Connection, id: i32) -> sqlx::Result<AnyQueryResult>;
 
-    async fn payout(pool: &Pool<Db>, user_id: impl Into<UserId> + Send, mut payout: i64) -> i64 {
+    async fn payout(
+        pool: &Pool<Db>,
+        user_id: impl Into<UserId> + Send,
+        mut base_payout: i64,
+    ) -> i64 {
         let mut tx = pool.begin().await.unwrap();
 
         let effects = Self::get_effects(&mut *tx, user_id).await.unwrap();
+
+        let mut final_payout = 0;
 
         for effect in effects {
             if effect
@@ -31,16 +38,24 @@ pub trait EffectsManager<Db: Database> {
                 .is_none_or(|expiry| expiry < Utc::now().naive_utc())
             {
                 Self::remove_effect(&mut *tx, effect.id).await.unwrap();
+
+                if effect.expiry.is_some() {
+                    continue;
+                }
             }
 
             let item = SHOP_ITEMS.get(&effect.item_id).unwrap();
 
-            payout = (item.effect_fn)(payout)
+            if matches!(item.category, ShopPage::Boost1) {
+                base_payout = (item.effect_fn)(base_payout)
+            }
+
+            final_payout += (item.effect_fn)(base_payout)
         }
 
         tx.commit().await.unwrap();
 
-        payout
+        final_payout
     }
 }
 
