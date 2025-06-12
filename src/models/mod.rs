4337af21df51173180d1b46e006282e486f84080
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-06-12 02:48:12 +0100

    mine update

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -257,8 +257,8 @@ pub trait Mining {
             return 0;
         }
 
-        const BASE_EXPONENT: f64 = 0.99;
-        const SCALING_CONST: f64 = 10.0;
+        const BASE_EXPONENT: f64 = 0.9;
+        const SCALING_CONST: f64 = 25.0;
 
         let base_value = (miners as f64).powf(BASE_EXPONENT);
         let prestige_multiplier = 1.0 + self.prestige() as f64 * 0.01;
