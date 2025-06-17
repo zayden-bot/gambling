bd93f54a6a67c83feb00f3fb3807ecb43abb1ec1
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-06-17 15:31:54 +0100

    update 4

--- src/commands/craft.rs
+++ src/commands/craft.rs
@@ -179,21 +179,30 @@ impl Commands {
 }
 
 async fn menu(ctx: &Context, interaction: &CommandInteraction, row: CraftRow) {
+    const SPACING: usize = 12;
+
+    let coal_amt = row.coal.format();
+    let gold_amt = row.gold.format();
+    let lapis_amt = row.lapis.format();
+
     let mut desc = format!(
-        "`{}` {}ㅤ          ㅤ`{}` {}
-        `{}` {}ㅤ          ㅤ`{}` {}
-        `{}` {}ㅤ          ㅤ`{}` {}
+        "`{}` {}ㅤ{}ㅤ`{}` {}
+        `{}` {}ㅤ{}ㅤ`{}` {}
+        `{}` {}ㅤ{}ㅤ`{}` {}
         `{}` {}",
-        row.coal.format(),
+        coal_amt,
         ShopCurrency::Coal,
+        " ".repeat(SPACING - coal_amt.len()),
         row.iron.format(),
         ShopCurrency::Iron,
-        row.gold.format(),
+        gold_amt,
         ShopCurrency::Gold,
+        " ".repeat(SPACING - gold_amt.len()),
         row.redstone.format(),
         ShopCurrency::Redstone,
-        row.lapis.format(),
+        lapis_amt,
         ShopCurrency::Lapis,
+        " ".repeat(SPACING - lapis_amt.len()),
         row.diamonds.format(),
         ShopCurrency::Diamonds,
         row.emeralds.format(),
